e04bc758d688d91a80d25960860989534a122ca5
jithamithra6304377545 <147488557+jithamithra6304377545@users.noreply.github.com>
2023-12-10 10:56:29 +0530

    #include<stdio.h>
    int main()
    {
       int x,y,var;
        scanf("%d%d",&x,&y);
        var=x*5;
        if(var>=y)
        {
        printf("YES");
        }
        else
        {
        printf("NO");
        }
    }

---
+++ Reach_Home.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+   int x,y,var;
+    scanf("%d%d",&x,&y);
+    var=x*5;
+    if(var>=y)
+    {
+    printf("YES");
+    }
+    else
+    {
+    printf("NO");
+    }
+}
